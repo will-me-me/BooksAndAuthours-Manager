c73ee3e82f51d88a89beff00d959d984f6860c11
“willy <[email protected]>
2024-06-14 12:02:10 +0300

    initial commit

---
+++ src/dashboard/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="" style="display: flex; justify-content: center; align-items: center; height: 100vh">
+        <UserLogin />
+    </div>
+</template>
+<script>
+import UserLogin from '~/components/UserLogin.vue';
+export default {
+    layout: ' default  ',
+
+    components: {
+        UserLogin,
+    },
+    data() {
+        return {
+            //
+        };
+    },
+};
+</script>
